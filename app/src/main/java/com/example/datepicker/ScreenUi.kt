8f5ef28dcdcaf59e6bb7df21f6d1967d007e35b7
asino-nelson <asinonelson276@gmail.com>
2023-02-04 22:06:06 +0300

    Calendar app

---
+++ app/src/main/java/com/example/datepicker/ScreenUi.kt
@@ -0,0 +1,85 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.example.datepicker
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.maxkeppeker.sheets.core.models.base.rememberSheetState
+import com.maxkeppeler.sheets.calendar.CalendarDialog
+import com.maxkeppeler.sheets.calendar.models.CalendarConfig
+import com.maxkeppeler.sheets.calendar.models.CalendarSelection
+import com.maxkeppeler.sheets.calendar.models.CalendarStyle
+import com.maxkeppeler.sheets.clock.ClockDialog
+import com.maxkeppeler.sheets.clock.models.ClockConfig
+import com.maxkeppeler.sheets.clock.models.ClockSelection
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+@Composable
+fun MainScreen(){
+
+    // CALENDAR
+
+    val calendarState = rememberSheetState()
+    val selectedDates = remember{ mutableStateOf<List<LocalDate>>(listOf()) }
+    val disableDates = listOf(
+        LocalDate.now().minusDays(7),
+        LocalDate.now().minusDays(12),
+        LocalDate.now().plusDays(3),
+    )
+
+    CalendarDialog(
+        state = calendarState,
+        config = CalendarConfig(
+            yearSelection = true,
+            monthSelection = true,
+            style = CalendarStyle.MONTH,
+            disabledDates = disableDates
+        ),
+        selection = CalendarSelection.Dates{ newDates ->
+            selectedDates.value = newDates
+        }
+    )
+
+    // CLOCK
+
+    val clockState = rememberSheetState()
+    val selectedTime = remember{ mutableStateOf<LocalDateTime?>(null) }
+    ClockDialog(
+        state = clockState,
+        config = ClockConfig(
+            is24HourFormat = false
+        ),
+        selection = ClockSelection.HoursMinutesSeconds { hours, minutes, seconds ->
+            selectedTime.value
+        }
+    )
+
+
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(100.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(onClick = { calendarState.show() }) {
+            Text(text = "Date Picker")
+        }
+        Button(onClick = { clockState.show() }) {
+            Text(text = "Time Picker")
+        }
+    }
+}
